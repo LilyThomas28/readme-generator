cf59b4faa6f1b8fcae1b50fed304577b3fa94075
Lily Thomas <lilylynettethomas@gmail.com>
2022-01-23 13:33:34 -0800

    Added code for function to test

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,34 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: import the generateMarkdown function from utils/generateMarkdown.js
+// fs.readFile('./utils/generateMarkdown.js', 'utf8', (error, data) =>
+//   error ? console.error(error) : console.log(data)
+// );
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create a function to initialize app
-function init() {}
+const questions = inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title?',
+      name: 'title',
+    },
+  ])
+  function writeFile(file, data) {
+    fs.writeFile(file, data, (err) => {
+        err ? console.log(err) : console.log('Success!')
+    });
+};
+
+// TODO: Create a function to initialize app??
+function init() {
+    questions
+    .then(function (input) {
+        console.log(input)
+        writeFile("README.md", generateMarkdown(input));
+    })
+}
 
 // Function call to initialize app
 init();
